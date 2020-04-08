c99ae2e1dfa08d89e4a8bcdc74d228ec05b5119e
Enzo Panettieri <genzop@gmail.com>
2020-04-08 13:40:06 -0300

    Finished section 10

---
+++ Section 10/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <app-header :count="quotes.length" :max="max"></app-header>
+        <app-quote-form @quoteSaved="addQuote"></app-quote-form>
+        <app-quote-list :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-list>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a quote to delete it</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue';
+    import QuoteForm from './components/QuoteForm.vue';
+    import QuoteList from './components/QuoteList.vue';
+
+    export default {
+        data: function() {
+            return {
+                max: 10,
+                quotes: [
+                    'Just a simple quote',
+                ]
+            };
+        },
+        methods: {
+            addQuote(quote) {
+                if(this.quotes.length >= this.max) {
+                    return alert('You have to delete some quotes to add more!');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            appHeader: Header,
+            appQuoteForm: QuoteForm,
+            appQuoteList: QuoteList,
+        },
+    }
+</script>
+
+<style>
+</style>
---
+++ Section 10/src/components/QuoteList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="row">
+        <app-quote v-for="(quote, index) in quotes" :key="quote" @click.native="deleteQuote(index)">
+            <h2>{{ quote }}</h2>
+        </app-quote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue';
+
+    export default {
+        props: {
+            quotes: Array
+        },
+        methods: {
+            deleteQuote(index) {
+                this.$emit('quoteDeleted', index);
+            }
+        },
+        components: {
+            appQuote: Quote
+        }
+    }
+</script>
+
+<style>
+    .panel-body {
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color: #6e6e6e;
+    }
+
+    .quote {
+        cursor: pointer;        
+    }
+    .quote:hover {
+        background-color: #ffe2e2;
+    }
+</style>
